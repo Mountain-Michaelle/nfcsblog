572def440a17bddd2141f23f47e0a9a1a2b8522d
unknown <Mountain-Michaelle>
2023-11-20 04:33:56 +0100

    Trying still

--- src/Components/Actions/auth.js
+++ src/Components/Actions/auth.js
@@ -125,7 +125,7 @@ export const reset_password = (email) => async dispatch => {
 }
 
 
-export const reset_password_confirm =(uid, token, new_password, re_new_password) => async dispatch => {
+export const reset_password_confirm = (uid, token, new_password, re_new_password) => async dispatch => {
 
     const headers = {
         'Content-Type': 'application/json',
